eb525998ae6d3cb2e344f9f170928bc07c631c10
Anish Srivastava <siranish97@gmail.com>
2021-10-26 14:45:15 +0530

    second

--- jsFolder/test1.js
+++ jsFolder/test1.js
@@ -1,7 +1,20 @@
+
+
+// variable and datatypes
+/* Assignement
+   */
+
+
 let country="India";
 let continent="Asia";
-let population="130 Crorre";
+let population=112356565;
+let isIsland = false;
+let language; 
 
 console.log(country);
 console.log(continent);
 console.log(population);
+console.log(typeof country);
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
