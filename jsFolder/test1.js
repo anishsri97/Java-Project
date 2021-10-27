72824d42cc8004084dc15605df45677b31a96aa9
Anish Srivastava <siranish97@gmail.com>
2021-10-27 10:15:56 +0530

    new commit

--- jsFolder/test1.js
+++ jsFolder/test1.js
@@ -18,3 +18,28 @@ console.log(typeof country);
 console.log(typeof isIsland);
 console.log(typeof language);
 console.log(typeof population);
+
+
+
+//String 
+const name="Anish Srivastava";
+const dob = 1996;
+const year=2021;
+const job = "Prgrammer Analyst";
+console.log(name +" is "+ job + " and age is "+ (year-dob));
+
+//loop
+
+
+for(var i=0;i<10;i++)
+{
+    console.log("hello world");
+}
+
+var a=11;
+
+//conditional statement
+
+if(a%2==0)console.log("Even");
+else if(a%2!=0)console.log("odd");
+else console.log("hello number");
