d5eeaecb30b70dcf40170e70be72c53f29041958
Anish Srivastava <siranish97@gmail.com>
2021-11-11 16:24:07 +0530

    new commit

---
+++ 100days/buy.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class buy 
+{
+   public static void main(String args[])
+   {
+     int a[]={1,2,3,4,5,6,7,8,9,0};
+     List l = Arrays.asList(a);
+     System.out.println("Maximum = "+Collections.min(l));
+     
+   }    
+}
